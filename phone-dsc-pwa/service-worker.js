ca656c9111794a7465d884a791c3407d575de70f
greasygpt85 <austinbox85@gmail.com>
2026-01-01 14:43:28 -0600

    Add service worker cache and secure registration

---
+++ phone-dsc-pwa/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'phone-dsc-cache-v2';
+const asset = (path) => new URL(path, self.location).toString();
+const OFFLINE_URL = asset('index.html');
+const PRECACHE_ASSETS = [asset('./'), OFFLINE_URL];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+        return response;
+      })
+      .catch(() => caches.match(request).then((cached) => cached || caches.match(OFFLINE_URL)))
+  );
+});
